16207e65a08ae0744ef78096166ec9e39e61df98
Gagan Deepak R <gagan.databee@gmail.com>
2022-08-30 18:08:55 +0530

    added self_test

--- MPU6050/RaspberryPi/MPU6050.py
+++ MPU6050/RaspberryPi/MPU6050.py
@@ -224,3 +224,43 @@ class MPU6050:
             GYRO_ZOUT_data = self.bus.read_i2c_byte_data(mpu6050.GYRO_XOUT_H+4)
         
         return {'x': GYRO_XOUT_data, 'y': GYRO_YOUT_data, 'z': GYRO_ZOUT_data}
+
+    
+    def self_test(self):
+        """
+        Self test of mpu6050
+        """
+
+
+
+    def self_test_accelerometer(self):
+        '''
+        Self Testing accelerometer for checking difference between factory trim values vs actuals
+        '''
+        
+
+
+    
+    def self_test_gyroscope(self):
+        """
+        Self Testing gyroscope for checking difference between factory trim values vs actuals
+        """
+        gyro_with_self_test_x = 
+        gyro_with_self_test_y =
+        gyro_with_self_test_z = 
+        gyro_with_self_test = 
+        self_test_response = gyro_with_self_test - gyro_without_self_test
+
+        diff = (self_test_response-factory_trim_value)/factory_trim_value
+
+    def get_gyro_ft_value(self,value=0):
+        """
+        finds the ft value of the gyroscope
+        """
+        return 25*131*pow(1.046,value-1)
+
+    def get_accel_ft_value(self,value=0):
+        """
+        finds the ft value of the accel
+        """
+        return (4096*0.34*pow(0.92,((value-1)/30)))/0.34
